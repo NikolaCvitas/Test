57bcadd3f318cdd2b088d3f4fc60b8a19c198cfa
Nikola Cvitaš <nikola.cvitas@btb.hr>
2018-11-23 13:04:46 +0100

    Added new method.

--- Test/src/hr/nikola/text/TextUtil.java
+++ Test/src/hr/nikola/text/TextUtil.java
@@ -14,7 +14,7 @@ public class TextUtil {
         for ( int i = 0; i < p_input.length(); ++i ) {
             c = p_input.charAt( i );
 
-            if ( c == 'È' || c == 'È' ) {
+            if ( c == 'Æ' || c == 'È' ) {
                 r += "C";
             }
             else if ( c == 'æ' || c == 'è' ) {
@@ -26,14 +26,64 @@ public class TextUtil {
             else if ( c == 'Š' ) {
                 r += "S";
             }
-            else if ( c == 'œ' || c == 'š' ) {
+            else if (  c == 'š' ) {
                 r += "s";
             }
-            else if ( c == 'Ž' || c == '' ) {
+            else if ( c == 'Ž' ) {
                 r += "Z";
             }
-            else if ( c == 'ž' || c == 'Ÿ' ) {
+            else if ( c == 'ž'  ) {
                 r += "z";
+            }
+            else {
+                r += c;
+            }
+
+        }
+
+        System.out.println( "r:" + r );
+
+        return r;
+    }
+    
+    /**
+     * Method replaces croatian diacritic signs with unicode characters.
+     * @param p_input
+     * @return
+     */
+    private String replaceCroatianCharsWithUnicode( String p_input ) {
+        String r = "";
+        char c;
+
+        for ( int i = 0; i < p_input.length(); ++i ) {
+            c = p_input.charAt( i );
+
+            if ( c == 'Æ' ) {
+                r += "\u0106";
+            }
+            else if ( c == 'È' ) {
+                r += "\u010C";
+            }
+            else if ( c == 'æ' ) {
+                r += "\u0107";
+            }
+            else if ( c == 'è' ) {
+                r += "\u010d";
+            }
+            else if ( c == 'Ð' ) {
+                r += "\u0110";
+            }
+            else if ( c == 'Š' ) {
+                r += "\u0160";
+            }
+            else if ( c == 'š' ) {
+                r += "\u0161";
+            }
+            else if ( c == 'Ž' ) {
+                r += "\u017D";
+            }
+            else if ( c == 'ž' ) {
+                r += "\u017e";
             }
             else {
                 r += c;
