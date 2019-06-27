5a5882de5a5141ddf1e00df6408043be203914e1
Nikola Cvitaš <nikolacvitas@gmail.com>
2019-06-27 23:36:45 +0200

    ExamplePane

--- Test/src/hr/nikola/swing/joption/ExamplePane.java
+++ Test/src/hr/nikola/swing/joption/ExamplePane.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.util.ArrayList;
 
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -12,6 +13,13 @@ import javax.swing.JOptionPane;
 import javax.swing.Timer;
 
 public class ExamplePane {
+	
+	private static ArrayList<String> lista = new ArrayList<String>();
+	
+	private static final int TIME = 1000;
+	
+	private static final int MULTIPLE = 3;
+	
 	
 	
 	public final static boolean showConfirmDialogWithTimeout(Object params, String title, int timeout_ms) {
@@ -50,7 +58,7 @@ public class ExamplePane {
 	    // example usage
 	    String message = "U tijeku je izvoz podataka u PDF";
 	    JLabel lbmsg = new JLabel(message);
-	    boolean result = showConfirmDialogWithTimeout(lbmsg, "Izvoz u PDF", 10 * 1000);
+	    boolean result = showConfirmDialogWithTimeout(lbmsg, "Izvoz u PDF", MULTIPLE  * TIME);
 
 	    if (result == false) {
 	    	System.out.println(message + " cancel is selected");
@@ -62,11 +70,33 @@ public class ExamplePane {
 	    }
 	    
 	}
+	
+	private static void fillList() {
+		
+		String name ="ABC";
+		
+		lista.add(name);
+		
+		String name2 ="ABC2";
+		
+		lista.add(name2);
+		String name3 ="ABC3";
+		
+		lista.add(name3);
+		String name4 ="ABC4";
+		
+		lista.add(name4);
+	}
 	    
 	    
 	    public static void main(String[] args) {
 
-	    	start();
+	    	fillList();
+	    	
+	    	for(String name : lista) {
+	    		start();
+	    	}
+	    	
 	    }
 
 }
